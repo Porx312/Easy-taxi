a7dde83cfbbe685ccdbe0be2ee0d4726e6ad7ae5
Jose <gregoriowhite333@gmail.com>
2026-03-03 00:21:57 +0100

    xd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
-
+import { Analytics } from "@vercel/analytics/next"
 const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
 
@@ -65,6 +65,8 @@ export default function RootLayout({
         <LanguageProvider>
           {children}
         </LanguageProvider>
+       <Analytics/>
+        
       </body>
     </html>
   )
